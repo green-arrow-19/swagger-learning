abb6056472a17ed5846133d0324f8cc0d40814d4
nakulGoyal19 <nakul19goyal@gmail.com>
2020-05-01 16:24:35 +0530

    initialized project from start.spring.io

---
+++ swagger/src/test/java/io/greenarrow19/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.greenarrow19.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9269b2fe314e47f44aabcdf38fb5b7da7d6da0c2
nakulGoyal19 <nakul19goyal@gmail.com>
2020-05-02 23:31:01 +0530

    Api-Swagger-Configs and Api Annotations

--- swagger/src/main/java/io/greenarrow19/swagger/AppController.java
+++ swagger/src/main/java/io/greenarrow19/swagger/AppController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * @author nakulgoyal
@@ -16,18 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app")
 public class AppController {
     
+    @ApiOperation(value = "Get All Apps",notes = "Get all apps saved in db",response = String.class)
     @GetMapping("/getAll")
     public String getApps(){
         return "Listed All Apps";
     }
     
+    @ApiOperation(value = "Add new App",notes = "Add new app in db",response = String.class)
     @PostMapping("/add/{appName}")
     public String addApp(@PathVariable String appName){
         return appName+" app added successfully";
     }
     
+    @ApiOperation(value = "Delete an App with appName",notes = "Delete an app i.e. saved in db",response = String.class)
     @DeleteMapping("/delete/{appName}")
-    public String deleteApp(@PathVariable String appName) {
+    public String deleteApp(@ApiParam(value = "App Name that needed to be deleted.", required = true) @PathVariable String appName) {
         return appName + " app deleted successfully";
     }
 }
--- swagger/src/main/java/io/greenarrow19/swagger/SwaggerApplication.java
+++ swagger/src/main/java/io/greenarrow19/swagger/SwaggerApplication.java
@@ -1,10 +1,13 @@
 package io.greenarrow19.swagger;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,8 +22,22 @@ public class SwaggerApplication {
 				.select()
 //				.paths(PathSelectors.ant("/app/*"))
 				.apis(RequestHandlerSelectors.basePackage("io.greenarrow19"))
-				.build();
+				.build()
+				.apiInfo(apiDetails());
 	}
+	
+	public ApiInfo apiDetails(){
+		return new ApiInfo("App Api",
+		                   "Learning Swagger Implementation",
+		                   "1.0",
+		                   "Free to use",
+		                   new springfox.documentation.service.Contact("Nakul Goyal","http://github.com/nakulGoyal19"
+				                   ,"..."),
+		                   "...",
+		                   "...",
+		                   Collections.emptyList());
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerApplication.class, args);
 	}
